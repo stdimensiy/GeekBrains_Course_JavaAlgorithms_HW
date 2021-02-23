855a2c7859682afd39d568cd82121549f1c33ad7
StDimensiy <stdimensiy@yandex.ru>
2021-02-24 00:45:16 +0300

    Старт выполнения домашнего задания к уроку №2

---
+++ src/main/java/ru/geekbrains/javaalgoritms/lesson2/Std_HW_Lesson2.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.javaalgoritms.lesson2;
+
+/**
+ * Сourse: java algorithms
+ * Faculty of Geek University Android Development
+ *
+ * @Author Student Dmitry Veremeenko aka StDimensiy
+ * Group 24.12.2020
+ * <p>
+ * HomeWork for lesson 2
+ * Created 23.02.2021
+ * v 1.0
+ */
+public class Std_HW_Lesson2 {
+    public static void main(String[] args) {
+
+    }
+}
