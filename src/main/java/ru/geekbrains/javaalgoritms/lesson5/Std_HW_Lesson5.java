0031203b03a63e5a169c28d8977d877d54c19f57
StDimensiy <stdimensiy@yandex.ru>
2021-02-28 02:27:18 +0300

    Старт выполнения домашнего задания к уроку №5

---
+++ src/main/java/ru/geekbrains/javaalgoritms/lesson5/Std_HW_Lesson5.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.javaalgoritms.lesson5;
+
+/**
+ * Сourse: java algorithms
+ * Faculty of Geek University Android Development
+ *
+ * @Author Student Dmitry Veremeenko aka StDimensiy
+ * Group 24.12.2020
+ * <p>
+ * HomeWork for lesson 5
+ * Created 28.02.2021
+ * v 1.0
+ */
+public class Std_HW_Lesson5 {
+}
