e3a0f27a40ebee7e24d080b33a4d84f1acf5190d
StDimensiy <stdimensiy@yandex.ru>
2021-03-01 16:14:11 +0300

    Старт выполнения домашнего задания к уроку №6

---
+++ src/main/java/ru/geekbrains/javaalgoritms/lesson6/Std_HW_Lesson6.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.javaalgoritms.lesson6;
+
+/**
+ * Сourse: java algorithms
+ * Faculty of Geek University Android Development
+ *
+ * @Author Student Dmitry Veremeenko aka StDimensiy
+ * Group 24.12.2020
+ * <p>
+ * HomeWork for lesson 6
+ * Created 01.03.2021
+ * v 1.0
+ */
+public class Std_HW_Lesson6 {
+
+}
