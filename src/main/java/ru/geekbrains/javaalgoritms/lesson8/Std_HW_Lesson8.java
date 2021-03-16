ab3a037bcad47d94d5ab4914c14a5379fe2ece71
StDimensiy <stdimensiy@yandex.ru>
2021-03-16 11:29:48 +0300

    Старт выполнения домашнего задания к уроку №8

---
+++ src/main/java/ru/geekbrains/javaalgoritms/lesson8/Std_HW_Lesson8.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.javaalgoritms.lesson8;
+
+/**
+ * Сourse: java algorithms
+ * Faculty of Geek University Android Development
+ *
+ * @Author Student Dmitry Veremeenko aka StDimensiy
+ * Group 24.12.2020
+ * <p>
+ * HomeWork for lesson 8
+ * Created 15.03.2021
+ * v 1.0
+ */
+public class Std_HW_Lesson8 {
+
+}
